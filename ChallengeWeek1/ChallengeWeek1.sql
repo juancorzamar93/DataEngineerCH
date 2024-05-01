2ba88b1eb62e2a986fa788415b1369dfb3aef260
Juan Cortez Zamar <106788005+juancorzamar93@users.noreply.github.com>
2024-05-01 00:04:35 -0300

    third commit

--- ChallengeWeek1/ChallengeWeek1.sql
+++ ChallengeWeek1/ChallengeWeek1.sql
@@ -70,5 +70,31 @@ where calls.pickedup = 1
 group by agents.name
 order by agents.name ASC;
 
---EXERCISES NUMBER 7:
+--EXERCISE NUMBER 7:
+--Escribir una consulta que extraiga dos métricas del desempeño de los agentes de ventas que le interesan a su empresa: 
+--1)Para cada agente, cuántos segundos en promedio les toma vender un producto cuando tienen éxito.
+--2)Para cada agente, cuántos segundos en promedio permanecen en el teléfono antes de darse por vencidos cuando no tienen éxito.
 
+SELECT a.name, 
+  	SUM( CASE
+			WHEN productsold = 0 THEN duration
+			ELSE 0
+		END)/SUM(
+		CASE
+			WHEN productsold = 0 THEN 1
+			ELSE 0
+		END)
+ AS avgWhenNotSold,
+ 	SUM(
+		CASE 
+			WHEN productsold = 1 THEN duration
+			ELSE 0
+		END)/SUM(
+			CASE WHEN productsold = 1 THEN 1
+			ELSE 0
+		END)
+ AS avgWhenSold
+FROM calls c
+JOIN agents a ON c.agentid = a.agentid
+GROUP BY a.name
+ORDER BY 1;
